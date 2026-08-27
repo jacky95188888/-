6184dec692c4054e22ed7309bc156558407f8e98
jacky95188888 <z2ykhmryg5@privaterelay.appleid.com>
2026-08-27 09:48:29 +0800

    feat: load Tianheng V9 LINE entry

--- tianheng-v3.js
+++ tianheng-v3.js
@@ -5,6 +5,7 @@
   var V6='https://raw.githubusercontent.com/jacky95188888/-/630018505be94199c15f0313f83ecf7462217805/tianheng-v6.js';
   var V7='https://raw.githubusercontent.com/jacky95188888/-/3b7c6f52de793a1de225ac7ca4ee2d6638f1c62b/tianheng-v7.js';
   var V8='https://raw.githubusercontent.com/jacky95188888/-/83e9ba1a669877536a529ccc09ed5fa3ccf5cd29/tianheng-v8.js';
+  var V9='https://raw.githubusercontent.com/jacky95188888/-/90c440619113c3bf00aab74b8b6463f7673b2271/tianheng-v9-line.js';
   function load(src,done){
     var s=document.createElement('script');
     s.src=src;
@@ -13,5 +14,5 @@
     s.onerror=function(){console.error('[天衡] 載入失敗：',src);(done||function(){})();};
     document.head.appendChild(s);
   }
-  load(V4,function(){load(V5,function(){load(V6,function(){load(V7,function(){load(V8);});});});});
+  load(V4,function(){load(V5,function(){load(V6,function(){load(V7,function(){load(V8,function(){load(V9);});});});});});
 })();
